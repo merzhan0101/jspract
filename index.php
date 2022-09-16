f61c1d9ced18fa23fa43cbb277b670f8950bc3ed
Құралай Мержан Әзімханұлы <khuralay.m@psu.local>
2022-09-16 12:24:53 +0600

    progressbar

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Прогресс бар</title>
+</head>
+<body>
+<!-- Прогресс бар -->
+    <div id="myProgress">
+        <div id="myBar">
+            <div id="label">10%</div>
+        </div>
+    </div>
+    <button onclick="move()">Click</button>
+
+<script src="assets/js/main.js"></script>
+</body>
+</html>
+
+
