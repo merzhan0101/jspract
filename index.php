f8dac96423039c5c95b7c810f18acd58f8aba51d
Құралай Мержан Әзімханұлы <khuralay.m@psu.local>
2022-09-16 12:15:39 +0600

    search

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Фильтрация на сайт</title>
+</head>
+<body>
+    <!-- Поисковик -->
+    <input type="text" onkeyup="myFunction()" id="myInput" placeholder="Search for places">
+    <ul id="myUL">
+        <li><a href="" class="header">A</a></li>
+            <li><a href="">Astana</a></li>
+            <li><a href="">Almaty</a></li>
+
+        <li><a href="" class="header">B</a></li>
+            <li><a href="">Burabay</a></li>
+            <li><a href="">Bayanaul</a></li>
+            <li><a href="">Besiktas</a></li>
+
+        <li><a href="" class="header">C</a></li>
+            <li><a href="">Central Asia</a></li>
+            <li><a href="">Costa Rica</a></li>
+    </ul>
+
+
+
+<script src="assets/js/main.js"></script>
+</body>
+</html>
