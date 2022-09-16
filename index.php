b8e3b75b9eb5f24f434816c01fb5b23a96809743
Құралай Мержан Әзімханұлы <khuralay.m@psu.local>
2022-09-16 12:36:56 +0600

    map

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Map</title>
+</head>
+<body>
+<!-- Карта -->
+    <div class="map">
+        <iframe src="https://maps.google.com/maps?width=700&amp;height=440&amp;hl=en&amp;q=Pavlodar%20%2CKazakhstan+(%D0%9D%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5)&amp;ie=UTF8&amp;t=&amp;z=15&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            <div class="karta"><small>Powered by <a href="https://embedgooglemaps.com/ru/"><a href="https://embedgooglemaps.com/ru/"></a> & <a href="https://bästacasinonutansvensklicens.se"><a href="https://bästacasinonutansvensklicens.se"></a></small>
+            </div>
+            <style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
+    </div><br />
+
+<script src="assets/js/main.js"></script>
+</body>
+</html>
+
+
